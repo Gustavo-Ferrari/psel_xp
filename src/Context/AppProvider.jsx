815e17aaf94b35afe49ddb260bd052d7dc1b44f7
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 12:16:01 -0300

    Cria estado global para manipulacao do saldo do cliente e passa os valores para o Provider

--- src/Context/AppProvider.jsx
+++ src/Context/AppProvider.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
+import { parse } from '../helpers';
 
 function AppProvider({ children }) {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const parsedBalance = parse('balance');
+
+  const [balance, setBalance] = useState(0 || parsedBalance);
+  localStorage.setItem('balance', balance || 0);
+
+  const VALUE = useMemo(
+    () => ({
+      balance,
+      setBalance,
+      parsedBalance,
+    }),
+    [
+      balance,
+      setBalance,
+      parsedBalance,
+    ],
+  );
+
+  return <AppContext.Provider value={VALUE}>{children}</AppContext.Provider>;
 }
 
 export default AppProvider;
