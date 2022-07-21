afeaa90cb0506e457df6ec5aedb35fc0a044480a
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 11:38:53 -0300

    Cria rota para login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {
+  Login,
+} from './Pages';
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
