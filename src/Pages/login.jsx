c217cce96a08f2adc17bac0bb37918f75fbcd81b
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 12:02:44 -0300

    Cria pasta Styles para manter os css de cada página Cria CSS da página de Login

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/login.css';
 import XPlogo from '../images/xp-logo.png';
 import loginData from '../validations/loginValidation';
 
