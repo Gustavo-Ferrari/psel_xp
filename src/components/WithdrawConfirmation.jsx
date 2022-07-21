bd6f36d0e2a411f8d529f6ef0cabdc0022ae6743
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 13:19:19 -0300

    cria componentes para confirmação de operações no saldo do cliente

---
+++ src/components/WithdrawConfirmation.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import AppContext from '../Context/AppContext';
+
+function DepositConfirmation({ closeConfirmation, withdraw }) {
+  const { setAddBalance } = useContext(AppContext);
+  return (
+    <div className="confirmation-background">
+      <div className="confirmation-container">
+        <div className="confirmation-title">
+          <h1>Deseja confirmar a operação?</h1>
+        </div>
+        <div className="confirmation-btn-container">
+          <button className="confirmation-cancel-btn" type="button" onClick={() => { closeConfirmation(); setAddBalance(''); }}>
+            Cancelar
+          </button>
+          <button className="confirmation-confirm-btn" type="button" onClick={() => { withdraw(); setAddBalance(''); }}>
+            Confirmar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DepositConfirmation;
+
+DepositConfirmation.propTypes = {
+  closeConfirmation: PropTypes.func.isRequired,
+  withdraw: PropTypes.func.isRequired,
+};
