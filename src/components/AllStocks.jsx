a41048142b35225c2209e288fc388725b6013db2
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 12:47:53 -0300

    Renderiza o componente de tabela de açoes, passando as acões do banco de dados

--- src/components/AllStocks.jsx
+++ src/components/AllStocks.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import AppContext from '../Context/AppContext';
+import StocksTable from './StocksTable';
 
 function AllStocks() {
+  const { allStocks } = useContext(AppContext);
+  const [isDisabled] = useState(true);
+
   return (
     <div>
       <h1 style={{ backgroundColor: 'grey', margin: '.5em', textAlign: 'center' }}>Disponíveis para Investir</h1>
+      <StocksTable stocks={allStocks} isDisabled={isDisabled} />
     </div>
   );
 }
