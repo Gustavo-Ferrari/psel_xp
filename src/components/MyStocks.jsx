ef3a8fdeeacd3c5fa785841d696ef1902e95dcb5
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 12:56:34 -0300

    Renderiza o componente de tabela de ações dentro do componente de ações	do cliente

--- src/components/MyStocks.jsx
+++ src/components/MyStocks.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { parse } from '../helpers';
+import StocksTable from './StocksTable';
 
 function MyStocks() {
+  const stocks = parse('myStocks');
+  const [isDisabled] = useState(false);
   return (
     <div>
       <h1 style={{ backgroundColor: 'grey', margin: '.5em', textAlign: 'center' }}>Minhas Ações</h1>
+      <StocksTable stocks={stocks} isVisible="none" isDisabled={isDisabled} />
     </div>
   );
 }
