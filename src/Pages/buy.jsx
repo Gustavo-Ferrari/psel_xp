19205725a6f753df2c41927bd2438869a5ab6b22
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 14:36:59 -0300

    Cria função para compra: Juntamente coma  função, foram feitas funções para validar a possibilidade de compra do cliente. A implementação da função de compra será feita em um componente de confirmação da operação

--- src/Pages/buy.jsx
+++ src/Pages/buy.jsx
@@ -3,14 +3,71 @@ import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import AppContext from '../Context/AppContext';
+import { parse, stringfy } from '../helpers';
 
 function Buy() {
-  const { selectedStock } = useContext(AppContext);
+  const {
+    selectedStock,
+    setMyStocks,
+    balance,
+    setBalance,
+    setAllStocks,
+  } = useContext(AppContext);
   const [buyQuantity, setBuyQuantity] = useState('');
   const navigate = useNavigate();
 
   const goBack = () => {
     navigate('/acoes');
+  };
+
+  const updateStock = (id, inventory) => {
+    const newInventory = inventory.map((el) => {
+      if (el.id === id) {
+        return {
+          ...el,
+          quantidade: el.quantidade - +buyQuantity,
+        };
+      }
+      return el;
+    });
+    setAllStocks(newInventory);
+    stringfy('stocks', newInventory);
+  };
+
+  const StockInMyAssetWallet = (stocks, id, valor, foundStock, inventory) => stocks.map((el) => {
+    const transactionApproved = el.id === id && balance >= +valor * +buyQuantity
+        && foundStock.quantidade >= +buyQuantity;
+    if (transactionApproved) {
+      setBalance(balance - +valor * +buyQuantity);
+      updateStock(id, inventory);
+      return {
+        ...el,
+        quantidade: el.quantidade + +buyQuantity,
+      };
+    }
+    return el;
+  });
+
+  const buyStock = ({ id, nome, valor }) => {
+    const inventory = parse('stocks');
+    const foundStock = inventory.find((el) => el.id === id);
+    setMyStocks((Stock) => {
+      const doesExist = Stock.some((el) => el.id === id);
+      if (doesExist) {
+        return StockInMyAssetWallet(Stock, id, valor, foundStock, inventory);
+      }
+      setBalance(balance - +valor * +buyQuantity);
+      updateStock(id, inventory);
+      return [
+        ...Stock,
+        {
+          id,
+          nome,
+          quantidade: +buyQuantity,
+          valor: +valor,
+        },
+      ];
+    });
   };
 
   return (
