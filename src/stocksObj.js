efdbba8a2873e03ad099b9299ea1487917f82543
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-21 12:28:32 -0300

    Cria array de objetos contendo as ações que serão usadas como banco de dados

---
+++ src/stocksObj.js
@@ -0,0 +1,70 @@
+const stocksObj = [
+  {
+    id: 1,
+    nome: 'CPTS11',
+    quantidade: 1000,
+    valor: 89.66,
+  },
+  {
+    id: 2,
+    nome: 'XPCI11',
+    quantidade: 1000,
+    valor: 93.45,
+  },
+  {
+    id: 3,
+    nome: 'KNCR11',
+    quantidade: 1000,
+    valor: 102.00,
+  },
+  {
+    id: 4,
+    nome: 'HGRU11',
+    quantidade: 1000,
+    valor: 118.77,
+  },
+  {
+    id: 5,
+    nome: 'BRCO11',
+    quantidade: 1000,
+    valor: 97.70,
+  },
+  {
+    id: 6,
+    nome: 'PVBI11',
+    quantidade: 1000,
+    valor: 91.60,
+  },
+  {
+    id: 7,
+    nome: 'RBRF11',
+    quantidade: 1000,
+    valor: 68.78,
+  },
+  {
+    id: 8,
+    nome: 'RBRR11',
+    quantidade: 1000,
+    valor: 97.87,
+  },
+  {
+    id: 9,
+    nome: 'LVBI11',
+    quantidade: 1000,
+    valor: 96.82,
+  },
+  {
+    id: 10,
+    nome: 'MCCI11',
+    quantidade: 1000,
+    valor: 97.70,
+  },
+  {
+    id: 11,
+    nome: 'XPML11',
+    quantidade: 1000,
+    valor: 94.16,
+  },
+];
+
+export default stocksObj;
