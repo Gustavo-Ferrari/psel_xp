ade0d590b875af0017d33f77aee918dc221941d2
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-22 21:38:03 -0300

    Implementa testes na tela de deposito

---
+++ src/test/deposit.test.js
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../../renderWithRouter';
+import context from './context';
+import AppContext from '../Context/AppContext';
+import { DepositWithdraw } from '../Pages';
+const renderDepositPage = () => {
+
+  renderWithRouter(
+    <AppContext.Provider value={context}>
+      <DepositWithdraw />
+    </AppContext.Provider>,
+  );
+};
+
+describe('Testa elementos da tela de venda', () => {
+  it('O HEADER deve ser renderizado', () => {
+    renderDepositPage();
+    const saldo = screen.getByRole('heading', {
+      level: 1,
+      name: /saldo em conta/i,
+    });
+    expect(saldo).toBeInTheDocument();
+  });
+
+  it('Os btns de DEPOSITO e RETIDADA devem estar na tela e desativados', () => {
+    renderDepositPage();
+    const depoBtn = screen.getByRole('button', { name: /depósito/i });
+    const withdrawBtn = screen.getByRole('button', { name: /retirada/i });
+    expect(depoBtn).toBeInTheDocument();
+    expect(withdrawBtn).toBeInTheDocument();
+    expect(depoBtn).toBeDisabled();
+    expect(withdrawBtn).toBeDisabled();
+  });
+});
