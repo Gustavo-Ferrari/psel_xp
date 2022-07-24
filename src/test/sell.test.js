744ffb8335196b95a5cb2106a37179de450edec7
Gustavo Ferrari <gustavod.ferrari@gmail.com>
2022-07-24 14:43:12 -0300

    Implementa mais testes unitários na tela de venda de ação

--- src/test/sell.test.js
+++ src/test/sell.test.js
@@ -4,17 +4,13 @@ import context from './helpers/context';
 import AppContext from '../Context/AppContext';
 import { Sell } from '../Pages';
 
-const renderSellPage = () => {
-  renderWithRouter(
-    <AppContext.Provider value={context}>
-      <Sell />
-    </AppContext.Provider>, ['/vender']
-  );
-};
-
 describe('Testa elementos da tela de venda', () => {
   it('O HEADER deve ser renderizado', () => {
-    renderSellPage();
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const saldo = screen.getByRole('heading', {
       level: 1,
       name: /saldo em conta/i,
@@ -23,7 +19,11 @@ describe('Testa elementos da tela de venda', () => {
   });
 
   it('O TITULO da página deve estar presente', () => {
-    renderSellPage();
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const title = screen.getByRole('heading', {
       level: 1,
       name: 'Comprar/Vender Ação',
@@ -32,25 +32,41 @@ describe('Testa elementos da tela de venda', () => {
   });
 
   it('O botão de venda deve estar na tela', () => {
-    renderSellPage();
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const sellBtn = screen.getByRole('button', { name: /vender/i });
     expect(sellBtn).toBeInTheDocument();
   });
 
-  it('O botão de voltar deve estar na tela', () => {
-    renderSellPage();
+  it('O botão de voltar deve estar na tela e ao ser clicado deve retornar a tela de acoes', () => {
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const goBackBtn = screen.getByRole('button', { name: /voltar/i });
     expect(goBackBtn).toBeInTheDocument();
   });
 
   it('O input de quantidade de venda deve estar na tela', () => {
-    renderSellPage();
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const valueInput = screen.getByRole('spinbutton');
     expect(valueInput).toBeInTheDocument();
   });
 
   it('Os dados da ação selecionada deve estar na tela', () => {
-    renderSellPage();
+    renderWithRouter(
+      <AppContext.Provider value={context}>
+        <Sell />
+      </AppContext.Provider>, ['/vender']
+    );
     const stockName = screen.getByRole('cell', { name: /ciel3/i });
     const stockQtd = screen.getByRole('cell', { name: /1000/i });
     const stockPrice = screen.getByRole('cell', { name: /69\.98/i });
